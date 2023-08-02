ecadfdd9dd58f83a67595cdf4be9dcb9f5cf0cb6
Biraj karki <birajkarki9849@gmail.com>
2023-08-02 13:30:13 +0545

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
+import logo from "./img/logo.png";
+import data from "./data";
+import Character from "./Components/Character";
+
 function App() {
+  const [characters] = useState(data);
+  const [activeChar, setActiveChar] = useState(characters[0]);
   return (
     <>
-      <div className="App">hello</div>
+      <div className="App">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="characters-con">
+          {characters.map((char) => {
+            const { id, image } = char;
+            return (
+              <li
+                key={id}
+                className={`${activeChar.id === id ? "active" : " "}`}
+                onClick={() => setActiveChar(char)}
+              >
+                <div className="characters"></div>
+                <img src={image} alt="" />
+              </li>
+            );
+          })}
+        </ul>
+        <Character {...activeChar} />
+      </div>
     </>
   );
 }
--- src/Components/Character.jsx
+++ src/Components/Character.jsx
@@ -1,5 +1,43 @@
-const Character = () => {
-  return <div>Character</div>;
-};
+import React, { useState } from "react";
+import CharacterMod from "./CharacterMod.module.css";
+
+function Character({ name, sounds, soundNames }) {
+  const [audio] = useState(new Audio());
+
+  const handleAudioPlay = (e) => {
+    audio.src = e.target.value;
+    audio.play();
+  };
+
+  return (
+    <div className={CharacterMod.SoundsCon}>
+      <div className={CharacterMod.NameCon}>
+        <h2>{name}</h2>
+      </div>
+      <div className={CharacterMod.Sounds}>
+        {sounds.map((sound, index) => {
+          return (
+            <button
+              className={CharacterMod.SoundsBtn}
+              value={sound}
+              key={index}
+              onClick={handleAudioPlay}
+            >
+              {soundNames[index]}
+            </button>
+          );
+        })}
+        {sounds.length === 0 && (
+          <p>
+            No sounds available for <span>{name}</span>
+          </p>
+        )}
+      </div>
+      <div className={CharacterMod.nam}>
+        <h5>Made by biraj karki</h5>
+      </div>
+    </div>
+  );
+}
 
 export default Character;
